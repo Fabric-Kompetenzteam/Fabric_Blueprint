2ff4c921a86ebdd6f0518e5d9e95e366708cf016
Oraylis-MS <m.schoeneberg@oraylis.de>
2025-11-14 09:25:30 +0100

    Committing 23 items from workspace 1d8f6de1-70b1-4989-ac36-66143ee32cfd

---
+++ CreateSchema.Notebook/notebook-content.sql
@@ -0,0 +1,43 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "synapse_pyspark"
+-- META   },
+-- META   "dependencies": {}
+-- META }
+
+-- CELL ********************
+
+CREATE SCHEMA IF NOT EXISTS raw
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+CREATE SCHEMA IF NOT EXISTS stage
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+CREATE SCHEMA IF NOT EXISTS curated
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
